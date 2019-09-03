d46efca621012699fb5b79fccc32de89236b7b04
RenatDoroshenko <renat.doroshenko@sitecore.com>
2019-09-03 11:45:33 +0300

    added logic that fix the issue at  "custom Editor button" and "editFrame" both

---
+++ src/Sitecore.Support.353234/Shell/Applications/ContentManager/ReturnFieldEditorValues/SetValues.cs
@@ -0,0 +1,58 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentManager.ReturnFieldEditorValues
+{
+    using Sitecore;
+    using Sitecore.Configuration;
+    using Sitecore.Data;
+    using Sitecore.Reflection;
+    using Sitecore.Shell.Applications.ContentEditor;
+    using Sitecore.Shell.Applications.ContentManager;
+    using Sitecore.Shell.Applications.ContentManager.ReturnFieldEditorValues;
+    using System.Web.UI;
+
+    public class SetValues
+    {
+        public void Process(ReturnFieldEditorValuesArgs args)
+        {
+            foreach (FieldInfo info in args.FieldInfo.Values)
+            {
+                Control control = Context.ClientPage.FindSubControl(info.ID);
+                if (control != null)
+                {
+                    string str;
+                    if (control is IContentField)
+                    {
+                        string[] values = new string[] { (control as IContentField).GetValue() };
+                        str = StringUtil.GetString(values);
+                    }
+                    else
+                    {
+                        str = StringUtil.GetString(ReflectionUtil.GetProperty(control, "Value"));
+                    }
+                    if (str != "__#!$No value$!#__")
+                    {
+                        string str2 = info.Type.ToLowerInvariant();
+                        if ((str2 == "rich text") || (str2 == "html"))
+                        {
+                            char[] trimChars = new char[] { ' ' };
+                            str = str.TrimEnd(trimChars);
+                        }
+                        foreach (FieldDescriptor descriptor in args.Options.Fields)
+                        {
+                            if (!(descriptor.FieldID == info.FieldID))
+                            {
+                                continue;
+                            }
+                            ItemUri uri = new ItemUri(info.ItemID, info.Language, info.Version, Factory.GetDatabase(descriptor.ItemUri.DatabaseName));
+                            if (descriptor.ItemUri == uri)
+                            {
+                                // added logic that encode "&" symbol
+                                str = str.Replace("&", "&amp;");
+                                descriptor.Value = str;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
